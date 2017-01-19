67be4d5a97c9d8ec2b9018c3d37360f95cc7349c
reimerjonas <jonas.reimer@elev.ga.lbs.se>
2017-01-19 13:40:07 +0100

    helloworld

---
+++ GitHubTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyProgram myMain = new MyProgram();
+            myMain.Run();
+        }
+    }
+}
